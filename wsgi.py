9f231c3f9443d2f483921d9525f6eb95646c8467
lizeyan <zy-li14@hotmail.com>
2017-08-08 11:00:16 +0800

    finish

--- wsgi.py
+++ wsgi.py
@@ -12,8 +12,9 @@ def application(environ, start_response):
     @param start_response: http response header
     @return body of html document
     """
-    path = os.path.realpath(os.path.join(HOME, environ['PATH_INFO']))
     body = b""
+    path = os.path.realpath(os.path.join(HOME, environ['PATH_INFO'].lstrip("/")))
+    rel = os.path.relpath(path, HOME)
     if not os.path.exists(path):
         start_response('200 OK', [('Content-Type', 'text/html')])
         body += json.dumps([]).encode("utf-8")
@@ -27,12 +28,18 @@ def application(environ, start_response):
     else:
         start_response('200 OK', [('Content-Type', 'text/html')])
         results = []
-        for f in os.listdir(path) + [".."]:
+        for f in os.listdir(path):
             real_path = os.path.join(path, f)
+            if real_path == os.path.realpath(__file__):
+                continue
             rel_path = os.path.relpath(real_path, HOME)
             if os.path.isdir(real_path):
                 results.append({"name": f, "url": HOST + "/" + rel_path, "type": "dir"})
             else:
                 results.append({"name": f, "url": HOST + "/" + rel_path, "type": "file", "size": os.path.getsize(real_path)})
+        if rel != ".":
+            real_path = os.path.join(path, "..")
+            rel_path = os.path.relpath(real_path, HOME)
+            results.append({"name": os.path.basename(real_path), "url": HOST + "/" + rel_path, "type": "dir"})
         body += json.dumps(results).encode("utf-8")
     return [body]
